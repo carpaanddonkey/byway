2d0c66fa951f06da7cce232ec03581de59255203
延昊南 <yanhaonan@weizoom.com>
2016-05-26 02:34:12 +0800

    [测试]serverapi for concurrency testing

--- serverapi/orders/orders.py
+++ serverapi/orders/orders.py
@@ -72,16 +72,16 @@ def get_orders(request, order_id=None):
 			query_key = 'order:w'+str(window_id)+':s'+str(expect_status)
 
 			# from redis
-			# order_list = r.smembers(query_key)
-			# for order in order_list:
-			# 	content.append(eval(order))
+			order_list = r.smembers(query_key)
+			for order in order_list:
+				content.append(eval(order))
 
 			# from db
 			if not content:
 				order_list = order_utils.get_order_by_window(window_id, expect_status)
 				content = order_utils.orders_to_array(order_list)
-				# for order in order_list:
-				# 	r.sadd(query_key, order.to_dict())
+				for order in order_list:
+					r.sadd(query_key, order.to_dict())
 
 		elif query_type == 'deliver':
 			canteen_id = request.GET.get('canteen_id', None)

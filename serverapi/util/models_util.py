dc96eb0493ae4599cf472ce992e769a8bc341974
延昊南 <yhnkb@hotmail.com>
2016-05-30 22:13:20 +0800

    [新增]serverapi Customer的订单列表接口，订单信息添加window_name

--- serverapi/util/models_util.py
+++ serverapi/util/models_util.py
@@ -15,6 +15,12 @@ class OrderUtils(object):
 	def get_order_by_deliver(self, canteen_id, status):
 		return Order.objects.filter(canteen_id=canteen_id, status=status)
 
+	def get_order_by_customer(self, customer_id, is_finished=True):
+		if is_finished:
+			return Order.objects.filter(customer_id=Customer.objects.get(id=customer_id), status=ORDER_COMPLETED)
+		else:
+			return Order.objects.filter(customer_id=Customer.objects.get(id=customer_id), status__lt=ORDER_COMPLETED)
+
 	def orders_to_array(self, order_list):
 		order_arr = []
 		for order in order_list:

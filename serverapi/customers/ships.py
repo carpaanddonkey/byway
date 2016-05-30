fc28b1f86716d2dc3c0f24a027fde6b5eacd5e0b
延昊南 <yhnkb@hotmail.com>
2016-05-30 23:06:13 +0800

    [完善]serverapi 收货地址相关接口

--- serverapi/customers/ships.py
+++ serverapi/customers/ships.py
@@ -13,18 +13,22 @@ def ships(request):
 	if request.method == 'POST':
 		return post_ships(request)
 	elif request.method == 'GET':
-		pass
+		return get_ships(request)
 
 
 @login_required
 def post_ships(request):
 	ship_data = json.loads(request.body)
+	receiver_name = ship_data.get('receiver_name', '无')
+	phone = ship_data.get('phone', '')
 	dormitory_no = ship_data.get('dormitory_no', 0)
 	room_no = ship_data.get('room_no', 0)
 	address = ship_data.get('address', u'无')
 
 	ship_info = Ship()
 	ship_info.customer_id = Customer.objects.get(id=request.user.id)
+	ship_info.receiver_name = receiver_name
+	ship_info.phone = phone
 	ship_info.dormitory_no = dormitory_no
 	ship_info.room_no = room_no
 	ship_info.address = address
@@ -34,3 +38,10 @@ def post_ships(request):
 	content['status'] = 201
 	content['msg'] = u'操作成功'
 	return create_simple_response(201, json.dumps(content))
+
+
+@login_required
+def get_ships(request):
+	ship_list = Ship.objects.filter(customer_id=Customer.objects.get(id=request.user.id))
+	ship_items = [x.to_dict() for x in ship_list]
+	return create_simple_response(200, json.dumps(ship_items))
